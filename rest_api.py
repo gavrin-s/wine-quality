2e9cdb91c205f3e2f31f0858fe2f3af435b55033
Sergey Gavrin <gavrin_s@mail.ru>
2019-01-09 13:02:20 +0700

    Add comments

--- rest_api.py
+++ rest_api.py
@@ -11,10 +11,14 @@ app = Flask(__name__)
 
 @app.route('/predict', methods=['POST'])
 def predict():
+    """
+    Endpoint prediction wine quality
+    """
     json_ = request.json
     df = pd.DataFrame(json_, index=[0], columns=columns)
     df = df[columns]
 
+    # if there are gaps filling them
     nan_cols = df.columns[df.isnull().values[0]]
     if nan_cols.any():
         for col in nan_cols:
@@ -23,11 +27,11 @@ def predict():
                 continue
             df.loc[0, col] = filler[col]
 
+    # preprocessing and scaling
     map_ = {'red': 0, 'white': 1}
     df['color'] = df['color'].map(map_)
     df[logs] = df[logs].applymap(np.log)
     df[sqrts] = df[sqrts].applymap(np.sqrt)
-
     X = df.values.reshape(1, -1)
     X_scaled = scaler.transform(X)
 
@@ -36,6 +40,7 @@ def predict():
 
 
 if __name__ == '__main__':
+    # initialization of meta information
     columns = ['fixed acidity', 'volatile acidity', 'citric acid', 'residual sugar',
                'chlorides', 'free sulfur dioxide', 'total sulfur dioxide', 'density',
                'pH', 'sulphates', 'alcohol', 'color'
@@ -45,6 +50,7 @@ if __name__ == '__main__':
             'volatile acidity']
     sqrts = ['citric acid']
 
+    # create json to fill gap
     filler = {'fixed acidity': 7.0, 'volatile acidity': 0.29,
               'citric acid': 0.31, 'residual sugar': 3.0,
               'chlorides': 0.047, 'free sulfur dioxide': 29.0,
@@ -52,6 +58,7 @@ if __name__ == '__main__':
               'pH': 3.21, 'sulphates': 0.51,
               'alcohol': 10.3, 'quality': 6.0}
 
+    # load model and scaler
     scaler = joblib.load('models/scaler')
     model = joblib.load('models/model')
 
